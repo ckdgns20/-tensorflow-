566deb1c356912d86ae355477608e1eca4230a6c
ckdgns20 <158741515+ckdgns20@users.noreply.github.com>
2024-09-19 10:25:44 +0900

    Update Linear Regression 예제.py

--- Linear Regression 예제.py
+++ Linear Regression 예제.py
@@ -2,23 +2,24 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+# 훈련에 사용할 입력, 출력 값을 임의로 생성.
 X = np.linspace(2, 10, num=50) # 2에서 10까지 숫자 50개 생성
-Y = np.random.rand(50)*10 + 2 
-Y.sort()
+Y = np.random.rand(50)*10 + 2  # 50개의 난수를 생성해서 10을 곱하고 2를 더하곘다
+Y.sort() # y 값 정렬
 print('X = ', X)
 print('Y = ', Y)
-
 
 plt.plot(X, Y, 'ro')
 plt.show()
 
+# 입력, 출력 관계를 학습하기위한 선형식 정의
 W = tf.Variable(np.zeros(()), name='weight') # 가중치
 b = tf.Variable(np.zeros(()), name='bias')   # 편향
 
 def linear_regression(x):   # 입력값 x 에 대해 선형 방정식 정의
     return W*x + b
  
-def mean_square(y_pred, y):  # 예측 값(y_pred)과 실제 값(y) 간의 평균 제곱 오차 계산(손실 함수 부분)
+def mean_square(y_pred, y):  # 예측 값(y_pred)과 실제 값(y) 간의 평균 제곱 오차 계산(손실 함수 정의한 부분)
     return tf.reduce_mean(tf.square(y_pred - y))
 
 # 학습 파라미터 설정
@@ -36,7 +37,7 @@ for epoch in range(1, epochs + 1):
     # update W and b following gradients
     optimizer.apply_gradients(zip(gradients, [W, b]))
  
-    if epoch % 50 == 0: # 매 50 에포크마다 현재 손실 값과 가중치,편향을 출력
+    if epoch % 50 == 0: #  50 에포크마다 현재 손실 값과 가중치,편향을 출력
         print(f'{epoch} epoch : loss = {loss}, W = {W.numpy()}, b = {b.numpy()}')
 
 # 데이터 시각화부분
